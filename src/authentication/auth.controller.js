838e6da1e5c1f9a7e93628b43a7c30bd0bf5fe15
o0m4r <omarksm09@gmail.com>
2022-03-03 09:17:14 +0200

    encrypting methot, login route added

--- src/authentication/auth.controller.js
+++ src/authentication/auth.controller.js
@@ -1,5 +1,11 @@
 const User = require('./auth.MySql.js').User;
-
+const bcrypt = require('bcrypt')
+const salt = 8; //salt for password hashing algorithm
+/**
+ * 
+ * changing salt will lose 
+ * 
+ */
 const getAllUsers = async (req, res) => {
     let data = await User.findAll({});
     console.log(data);
@@ -7,7 +13,26 @@ const getAllUsers = async (req, res) => {
 }
 const addUser = async (req, res) => {
     const {id,username, password} = req.body;
-    await User.create({id,username, password});
+    bcrypt.hash(password, salt, async(err,hash)=> {
+        await User.create({id,username, password:hash});
+    })
+  
     res.json({msg: 'User created successfully',username: username});
 }
-module.exports ={getAllUsers ,addUser };
+
+const login = async (req, res) => {
+    
+    const {username,password} = req.body;
+    const user = await User.findOne({where: {username: username}})
+
+    // chck if password is correct 
+    
+    const matches = await bcrypt.compare(password, user.password)
+    if(matches){
+        res.json({user: user});
+    }
+    else res.json({msg:"Username or password is incorrect"})
+}
+
+
+module.exports = { getAllUsers, addUser, login };
--- src/authentication/userRoutes.routes.js
+++ src/authentication/userRoutes.routes.js
@@ -2,4 +2,5 @@ const router = require('express').Router();
 const authController = require('./auth.controller');
 router.get('/getAll',  authController.getAllUsers );
 router.post('/addUser', authController.addUser );
+router.post('/login',  authController.login );
 module.exports = router ;
