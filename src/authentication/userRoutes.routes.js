6a411f140546e8b5a0e0c28815bfd0304e180ea9
o0m4r <omarksm09@gmail.com>
2022-03-08 19:34:04 +0200

    validate JWT

--- src/authentication/userRoutes.routes.js
+++ src/authentication/userRoutes.routes.js
@@ -1,6 +1,11 @@
 const router = require('express').Router();
+const passport = require('passport');
+// Pass the global passport object into the configuration function
+require('../middleware/passport')(passport);
+// This will initialize the passport object on every request
+router.use(passport.initialize());
 const authController = require('./auth.controller');
-router.get('/getAll',  authController.getAllUsers );
+router.get('/getAll',passport.authenticate('jwt',{session: false}),  authController.getAllUsers );
 router.post('/addUser', authController.addUser );
 router.post('/login',  authController.login );
 module.exports = router ;
