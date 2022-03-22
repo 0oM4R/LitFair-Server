dfd8669d67a4bef604959a4455d48cc6941a1b36
MuhamedMagdi <m49dii@gmail.com>
2022-03-22 21:09:35 +0200

    Adding the routes for googleOAuth

--- src/authentication/userRoutes.routes.js
+++ src/authentication/userRoutes.routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 const auth = require('../middleware/passport').passport;
 const controller = require('./auth.controller');
 const { googleCallback, googleAuthenticate } = require('../middleware/googleOAuth');
+const { googleCallbackAddUser, googleAuthenticateAddUser } = require('../middleware/googleAddUser.js');
 
 router.get('/getAll', auth, controller.getAllUsers);
 router.post('/addUser', controller.addUser);
@@ -10,6 +11,9 @@ router.post('/login', controller.login);
 router.get('/logout', controller.logout);
 
 router.get('/auth/google', googleAuthenticate);
-router.get('/google/callback', googleCallback, controller.issueJwtGoogle);
+router.get('/google/callback', googleCallback,  controller.login);
+
+router.get('/adduser/google', googleAuthenticateAddUser);
+router.get('/google/callback/adduser', googleCallbackAddUser,  controller.addUser);
 
 module.exports = router;
