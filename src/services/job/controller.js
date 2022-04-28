dc3ded3f66ccc92b814a4ed6cb5dae348494d7bf
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-04-28 23:51:39 +0200

    merge

--- src/services/job/controller.js
+++ src/services/job/controller.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require('mongoose');
-const jobModel = require('./model-job').jobModel;
-const applicationModel = require('./model-job').appModel;
-
-class jobController {
-  constructor(dbURI) {
-    this.dbURI = dbURI;
-    mongoose
-      .connect(dbURI)
-      .then(() => console.log('Job database connected successfully'))
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-
-  async addJob(post) {
-    let data = post;
-    const toDate = new Date(data.date_posted + 'UTC');
-    data.date_posted = toDate == 'Invalid Date' ? new Date() : toDate;
-
-    const newJob = new jobModel(data);
-=======
 const {jobModel, appModel} = require('./model');
 
 
@@ -53,32 +30,20 @@ const {jobModel, appModel} = require('./model');
         throw new Error(`Can't get job::ERROR:${err}`);
     }
   }
->>>>>>> job
 
   async addJob(schema) {
     try {
-<<<<<<< HEAD
-      const response = await newJob.save();
-      return response;
-    } catch (e) {
-      throw new Error(`Can't Add new job e:${e}`);
-=======
       const saved = new jobModel(schema);
       
       const doc =  await saved.save();
       return doc;
     } catch (err) {
         throw new Error(`Can't Add new job::ERROR:${err}`);
->>>>>>> job
     }
   }
   
 
-<<<<<<< HEAD
-  async getJobs() {
-=======
   async editJob(id, schema) {
->>>>>>> job
     try {
       const doc = await jobModel.findByIdAndUpdate(id, schema, {  new: true });
       return doc;
@@ -87,39 +52,6 @@ const {jobModel, appModel} = require('./model');
     }
   }
 
-<<<<<<< HEAD
-  async getJob(id) {
-    try {
-      const response = await jobModel.findById(id).exec();
-      return response;
-    } catch (e) {
-      throw new Error(`Can't get job with ID:${id}-e:${e}`);
-    }
-  }
-
-  async editJob(id) {
-    try {
-      const response = await jobModel.findOneAndUpdate({ _id: id }, req.body, {
-        new: true,
-      });
-      return response;
-    } catch (e) {
-      throw new Error(`Can't edit job with ID:${id}-e:${e}`);
-    }
-  }
-
-  async deletJob(id) {
-    try {
-      const response = await jobModel.findByIdAndDelete({ _id: job_id });
-      return response;
-    } catch (e) {
-      throw new Error(`Can't delete job with ID:${id}-e:${e}`);
-    }
-  }
-}
-
-class applicationController {}
-=======
   async deleteJob(id){
     try{
         const doc = await jobModel.findByIdAndDelete(id);
@@ -190,7 +122,6 @@ class applicationController{
     }
   }
 }
->>>>>>> job
 
 module.exports = {
   jobController,
