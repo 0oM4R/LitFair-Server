ad70ceb95546fb03d57396cb73f79e70f353be53
Mohamed El-khamisi <mohammd.alkhamisi@gmail.com>
2022-06-02 15:32:35 +0200

    cors

--- src/app.js
+++ src/app.js
@@ -56,11 +56,23 @@ if(workspace != 'cupcake'){
 
 
 
-app.use(cors(
-  { 
-    origin: true,
-    credentials: true}
-));  
+// app.use(cors(
+//   { 
+//     origin: true,
+//     credentials: true}
+// ));  
+app.use((req, res, next)=>{
+  // const allowedOrigins = ['http://localhost:3000', 'http://46.101.189.190', 'https://46.101.189.190'];
+  const origin = req.headers.origin;
+  // if (allowedOrigins.includes(origin)) {
+  // }
+  res.set('Access-Control-Allow-Origin', origin);
+  res.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.set('Access-Control-Allow-Credentials', true);
+  return next();
+})
+
 app.use(cookies())
 app.use(express.json());
 app.use(morgan("dev"));
