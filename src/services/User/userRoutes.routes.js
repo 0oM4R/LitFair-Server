c54b6971fe5e8e984858938fc2c1525489b01824
o0m4r <omarksm09@gmail.com>
2022-06-15 14:24:46 +0200

    change password controller

--- src/services/User/userRoutes.routes.js
+++ src/services/User/userRoutes.routes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
 const { jwtStrategy, googleCallback, googleCallbackAddUser, googleAuthenticateAddUser } = require('../../middleware/passport');
 const loginGoogle = require('../../middleware/googleOauth');
-const { getAllUsers, addUser, login, logout, refreshJWT, googleLogin } = require('../User/controller-User');
+const { getAllUsers, addUser, login, logout, refreshJWT, googleLogin,changePassword  } = require('../User/controller-User');
 const { createSeekerProfile } = require('../seeker/controller-seeker');
 
 router.get('/getAll', jwtStrategy, getAllUsers);
 router.post('/addUser', addUser, createSeekerProfile);
 router.post('/login', login);
 router.delete('/logout', logout);
-
+router.put('/user/changePassword',jwtStrategy,changePassword)
 router.get('/jwtValidate', jwtStrategy, refreshJWT);
 
 router.post('/auth/google', loginGoogle, googleLogin);
